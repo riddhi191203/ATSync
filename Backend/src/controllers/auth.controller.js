d44303b28caebe5d28db9cc3e6ddc01243c8be28
riddhi191203 <riddhijain.rj38.com>
2026-05-19 17:11:13 +0530

    minor changes for deployment

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -88,7 +88,11 @@ async function loginUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none"
+    })
     res.status(200).json({
         message: "User loggedIn successfully.",
         user: {
